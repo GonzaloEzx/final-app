e4b041b43900a967b248ea42935e6ef81c976cac
Gonxa <Gonza@example.com>
2021-12-18 21:33:11 -0300

    creando login

--- Backend/blog/models.py
+++ Backend/blog/models.py
@@ -19,7 +19,6 @@ class Post(models.Model):
 		('draft', 'Draft'),
 		('published', 'Published'),
 	)
-
  # campos
 	category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
 	title = models.CharField(max_length=255)
